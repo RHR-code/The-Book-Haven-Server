a24ec71e767ac25ad95d5d46408b9da519831e4e
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-11-10 21:34:34 +0600

    all books and latest books api added

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
+const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(cors());
+app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.x3egp.mongodb.net/?appName=Cluster0`;
 
 app.get("/", (req, res) => {
@@ -22,10 +25,28 @@ async function run() {
     await client.connect();
 
     const bookDB = client.db("bookDB");
-    const books = bookDB.collection("books");
+    const booksCollection = bookDB.collection("books");
 
-    const result = await books.insertOne({ name: "RHR" });
-    console.log(result);
+    // all books api
+    app.get("/all-books", async (req, res) => {
+      const result = await booksCollection.find().toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // latest books api
+    app.get("/latest-books", async (req, res) => {
+      const result = await booksCollection
+        .find()
+        .sort({ createdAt: -1 })
+        .limit(6)
+        .toArray();
+      console.log(result);
+      res.send(result);
+    });
+
+    // const result = await .insertOne({ name: "RHR" });
+    // console.log(result);
 
     await client.db("admin").command({ ping: 1 });
     console.log(
