9975ea434e27d7710a9f79b83bf99c0d981846c9
RHR-code <{ID}+{username}@users.noreply.github.com>
2025-11-11 09:55:05 +0600

    update book api added

--- index.js
+++ index.js
@@ -64,8 +64,14 @@ async function run() {
       res.send(result);
     });
 
-    // const result = await .insertOne({ name: "RHR" });
-    // console.log(result);
+    // update book
+    app.patch("/update-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const update = { $set: req.body };
+      const result = await booksCollection.updateOne(query, update);
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
